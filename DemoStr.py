85fc1259fe6194021091f4b86b8517fcd94e369c
papasmf1 <papasmf1@gmail.com>
2024-05-16 16:42:19 +0900

    문자열 처리 연습

---
+++ DemoStr.py
@@ -0,0 +1,16 @@
+# DemoStr.py 
+#순회가능한 열거형식 
+strA = "python is very powerful"
+lst = [100, 200, 300]
+tp = (10, 20, 30)
+
+print(strA[:6])
+print(lst[1])
+print(tp[2])
+
+names = ["전우치","이순신","박문수"]
+for name in names:
+    print("안녕하세요 {0}님! 여름입니다.".format(name))
+    print("=" * 40)
+
+    

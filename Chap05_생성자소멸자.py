bec08dd8a2bdb65af6279185cb44dcda7ede19a3
papasmf1 <papasmf1@gmail.com>
2024-05-16 09:45:19 +0900

    클래스 상속 연습

--- Chap05_생성자소멸자.py
+++ Chap05_생성자소멸자.py
@@ -1,16 +1,17 @@
 # Chap05_생성자와소멸자.py 
-
 class MyClass:
+    #초기화 
     def __init__(self, value):
         self.value = value
         print("Instance is created! Value = ", value)
+    #소멸자 
     def __del__(self):
         print("Instance is deleted!")
 
 #인스턴스 생성 
 d = MyClass(10)
 #참조카운트를 1에서 0으로 만들면 소멸
-del d
+#del d
 
 print("전체 코드 실행 종료")
 
--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -1,11 +1,15 @@
 class Person:
     pass
+
 class Bird:
     pass
+
 class Student(Person):
     pass
 
+#인스턴스 생성 
 p, s = Person(), Student()
+
 print("p is instance of Person: ", isinstance(p, Person))
 print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))

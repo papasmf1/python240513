486f77e3c6e6decbc1e5f7d1c7d47d12c4c59b67
papasmf1 <papasmf1@gmail.com>
2024-05-17 09:41:30 +0900

    문자열 처리 연습1

--- DemoStr.py
+++ DemoStr.py
@@ -13,4 +13,32 @@ for name in names:
     print("안녕하세요 {0}님! 여름입니다.".format(name))
     print("=" * 40)
 
-    
+strA = "python is very powerful"
+strB = "파이썬은 강력해"
+print(len(strA))
+print(len(strB))
+print(strA.capitalize())
+print(strA.count("p"))
+print(strA.count("p", 7))
+print("demo.ppt".startswith("demo"))
+print("demo.ppt".endswith("ppt"))
+print(strA.upper())
+print("---숫자와 알파벳으로 구성---")
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+print("2580".isdecimal())
+
+print("---공백문자 제거---")
+data = "<<<  spam and ham  >>>"
+result = data.strip("<> ") 
+print(data)
+print(result)
+result2 = result.replace("spam", "spam egg")
+print(result2)
+print("---리스트로 변환---")
+lst = result2.split() 
+print(lst)
+print(":)".join(lst))
+
+
+
